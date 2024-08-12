52216bb0ba5bff7f73a480e48a2f514ca55606ff
pbicskei <53083249+pbicskei@users.noreply.github.com>
2024-08-12 21:57:16 +0000

    Update install-terraform-docs.sh to download to /tmp

--- install-terraform-docs.sh
+++ install-terraform-docs.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-# Update the package list to make sure we’re getting the freshest packages
+# Ensure the script exits immediately if a command exits with a non-zero status
+set -e
+
+# Update the package list to ensure we're getting the latest packages
 echo "Updating package list..."
 sudo apt-get update -y
 
-# Install prerequisites
+# Install prerequisites if not already installed
 echo "Installing prerequisites..."
 sudo apt-get install -y curl unzip
 
 # Define the version of terraform-docs you want to install
 TERRAFORM_DOCS_VERSION="v0.18.0"
 
+# Set the temporary file and extraction directory
+TEMP_DIR=$(mktemp -d)
+TERRAFORM_DOCS_TAR="$TEMP_DIR/terraform-docs.tar.gz"
+TERRAFORM_DOCS_BIN="$TEMP_DIR/terraform-docs"
+
 # Download terraform-docs binary from GitHub releases
 echo "Downloading terraform-docs..."
-curl -Lo /tmp/terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/$TERRAFORM_DOCS_VERSION/terraform-docs-$TERRAFORM_DOCS_VERSION-linux-amd64.tar.gz
+curl -Lo $TERRAFORM_DOCS_TAR https://github.com/terraform-docs/terraform-docs/releases/download/$TERRAFORM_DOCS_VERSION/terraform-docs-$TERRAFORM_DOCS_VERSION-linux-amd64.tar.gz
 
 # Extract the downloaded tar.gz file
 echo "Extracting terraform-docs..."
-tar -xzf /tmp/terraform-docs.tar.gz /tmp
+tar -xzf $TERRAFORM_DOCS_TAR -C $TEMP_DIR
 
 # Move the binary to /usr/local/bin for system-wide use
 echo "Installing terraform-docs..."
-sudo mv /tmp/terraform-docs /usr/local/bin/
+sudo mv $TERRAFORM_DOCS_BIN /usr/local/bin/terraform-docs
 
-# Cleanup the tar.gz file
+# Cleanup temporary files and directory
 echo "Cleaning up..."
-rm /tmp/terraform-docs.tar.gz
+rm -rf $TEMP_DIR
 
 # Verify installation
 echo "Verifying terraform-docs installation..."
